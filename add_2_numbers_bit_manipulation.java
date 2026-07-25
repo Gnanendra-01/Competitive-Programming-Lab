bebd5ba6abe6f0008e6b4e702a02a7b14563a069
Vavilapalli Gnanendra <gnanendravavilapalli@gmail.com>
2026-07-25 19:36:44 +0530

    Add bit manipulation method for number addition
    Implement addition of two numbers using bit manipulation.

---
+++ add_2_numbers_bit_manipulation.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+        while(b != 0){
+          int carry = a & b;
+          a = a ^ b;
+          b = carry << 1;
+        }
+        System.out.println(a);
+    }
+}
